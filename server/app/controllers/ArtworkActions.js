148102a4273c3daca832dd3b634f2fc21070baf7
Davido91390 <david.kabore@hotmail.fr>
2024-11-05 16:02:47 +0100

    artwork modification

--- server/app/controllers/ArtworkActions.js
+++ server/app/controllers/ArtworkActions.js
@@ -47,7 +47,7 @@ const readByAdmin = async (req, res, next) => {
 
 const add = async (req, res, next) => {
   const artwork = req.body;
-  artwork.user_id = 1;
+  artwork.user_id = req.body.user_id;
   try {
     const insertId = await tables.artwork.create(artwork);
     res.status(201).json({ insertId });
